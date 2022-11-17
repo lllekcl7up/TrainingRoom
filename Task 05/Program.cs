3da8e132fd05d6ecc50cfb81793a094bfb31c315
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-17 19:57:04 +0300

    Вычислить сумму, разность,произведение и частность двух чисел

---
+++ Task 05/Program.cs
@@ -0,0 +1,20 @@
+﻿//Даны два действительных числа x и у. Вычислить их сумму, разность, произведение и частное 
+
+double x = new Random().NextDouble(); // Работает от 0 до 1 
+double y = new Random().NextDouble();
+
+Console.WriteLine("Какие числа мы вычисляем");
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+Console.Write("Cумма чисел = ");
+Console.WriteLine(x + y);
+
+Console.Write("Разность чисел = ");
+Console.WriteLine(x - y);
+
+Console.Write("Произведение чисел = ");
+Console.WriteLine(x * y);
+
+Console.Write("Частность чисел = ");
+Console.WriteLine(x / y);

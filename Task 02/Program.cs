07bc470eaad4d4a47905502cf2befd965d0e7ae3
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-17 19:54:41 +0300

    Вывод на экран 5ти любых чисел

---
+++ Task 02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Составить программу вывода на экран "столбиком" пяти любых чисел веденных пользователем
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите четвертное число: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите пятое число: ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вы ввели: ");
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);

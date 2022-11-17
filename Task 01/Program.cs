18f5a0d7963f3797f67ae80b04ebec1db6cf2f44
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-17 19:52:31 +0300

    Вывод на экран четырех любых чисел

---
+++ Task 01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Составь программу вывода на экран в строку четырех любых чисел с тремя пробелами.
+
+int a = new Random().Next(55,900);
+int b = new Random().Next(55,900);
+int c = new Random().Next(55,900);
+int d = new Random().Next(55,900);
+
+Console.WriteLine($"Решение задачи: {a}  {b}  {c}  {d}");

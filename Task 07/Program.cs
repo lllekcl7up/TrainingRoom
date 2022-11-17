d1d43d1d52bd619efccd8bff510162e2e99d7a65
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-17 19:58:18 +0300

    Приветствуем Машу особым образом

---
+++ Task 07/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача: необходимо ввести имя, Машу поприветствовать особым образом.
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine()!;
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ВАУ - Маша, ты пришла!");
+}
+else
+{
+    Console.Write($"Привет, ");
+    Console.WriteLine(username);
+}

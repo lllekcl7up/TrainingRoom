3512936d65a53d01b4318a8735b19a11a1d223c2
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-17 19:57:44 +0300

    Вывод предедущего и последующего символа введеного пользователем

---
+++ Task 06/Program.cs
@@ -0,0 +1,6 @@
+﻿//Ввести любой символ и указать предыдущий и последующий символы. 
+
+Console.WriteLine("Введите любой символ: ");
+char x = Console.ReadLine()![0];
+Console.WriteLine("Предыдущий символ: {0}", (char)(x-1));
+Console.WriteLine("Следующий символ: {0}", (char)(x+1));
